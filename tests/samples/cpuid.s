e288ab4887298f1fdbdcb933db16ae96d0e9f559
Dmitry Buvaylo <moxoron@gmail.com>
2021-08-24 19:09:43 +0300

    tests

---
+++ tests/samples/cpuid.s
@@ -0,0 +1,12 @@
+.text
+    write	"cpuid implemented number"
+    alloc	96
+    ldi		%r13, 0
+    cpuid	%r14, %r13, 0
+    write	"cpuid len %x64(r14)"
+    write	"cpuid loop"
+cpuid_loop:
+    cpuid	%r15, %r13, 0
+    write	"cpuid[%i64(r13)] = %x64(r15)"
+    reps.lt.d	%r13, %r14, cpuid_loop
+.end
