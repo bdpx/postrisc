93e5808e473506ffe20cc199f8a1e0a6f7067de0
Dmitry Buvailo <moxoron@gmail.com>
2023-06-17 18:19:16 +0300

    musl utils

---
+++ musl/build_lib.native.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+. ../utils/common.bash
+
+export ARCH=$(uname -m)
+export CLANG_PATH="/usr/bin"
+
+declare -a CLANG_OPTIONS=(
+)
+
+declare -a LINKER_OPTIONS=(
+)
+
+. ./build_lib.bash
