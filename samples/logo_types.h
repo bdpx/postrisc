3545ce43cfcdbf2f436d6e73af54d992d23b5916
Dmitry Buvailo <moxoron@gmail.com>
2023-10-20 20:15:03 +0300

    samples for postrisc compiler

---
+++ samples/logo_types.h
@@ -0,0 +1,16 @@
+#pragma once 
+
+#define SYSINFO_RIGHT  0
+#define SYSINFO_BOTTOM 1
+
+struct logo_info {
+      char *description;
+      char *name;
+      int default_is_ascii;
+      int sysinfo_position;
+      char *logo;
+      char *ascii_logo;
+      int ysize;
+      int ascii_ysize;
+      struct logo_info *next_logo;
+};
