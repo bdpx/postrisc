89ebf2877c94fb3488b18a3c28ab83433cc49356
Dmitry Buvailo <moxoron@gmail.com>
2023-10-20 20:05:39 +0300

    utils

---
+++ utils/cmake_debug.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+. ./common.bash
+
+BUILD_DIR=${ROOT_DIR}/postrisc_debug
+
+rm -rf ${BUILD_DIR}
+mkdir ${BUILD_DIR}
+
+cmake -G "Unix Makefiles" \
+    -DCMAKE_BUILD_TYPE=DebWithAsan \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -B ${BUILD_DIR} \
+    -S ${ROOT_DIR}/postrisc
